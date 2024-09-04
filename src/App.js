dd49a8a2f75ed1d661e0c5ce0ba2ba5242af9629
Henrietta <henrietta.doddamani@gmail.com>
2024-09-04 19:13:36 +0530

    updated header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import CarList from './CarList';
 import './App.css';
+import { Typography } from '@mui/material';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Available Cars</h1>
+        <h1>Only Cars</h1>
+        <Typography variant="h6">
+            Contact Amrith Poovanna : 8970312345 
+        </Typography>
       </header>
       <CarList />
     </div>
