2057b5b8eeacb0fcd6c6e5d4287ebca66bb3aea1
Henrietta <henrietta.doddamani@gmail.com>
2024-09-04 17:58:46 +0530

    Prepare for deployment

---
+++ src/CarList.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import CarTile from './CarTile';
+import { Grid } from '@mui/material';
+import cars from './cars.json'; // Import your car data
+import './CarTile.css'; 
+
+const CarList = () => {
+  return (
+    <Grid container spacing={4} className="car-list">
+      {cars.map(car => (
+        <Grid item xs={12} sm={6} md={4} key={car.id}>
+          <CarTile car={car} />
+        </Grid>
+      ))}
+    </Grid>
+  );
+};
+
+export default CarList;
